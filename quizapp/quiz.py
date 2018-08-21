d13e1e31563ee637d634ea4c63cd77f03bb9d6d6
yubaraj poudel <yubarajpoudel708@gmail.com>
2018-08-21 16:28:54 +0545

    speak disabled in quiz

--- quizapp/quiz.py
+++ quizapp/quiz.py
@@ -15,18 +15,18 @@ Simple quiz app developed by the python trainee group from the broadway
 import json
 import os
 import time
-import pyttsx
+#import pyttsx
 with open("qa.json", "r") as qa:
 	questionSet = qa.read()
 	# json saved in the file is jsonArray hence loads() this function converts it into list
 	questionsList = json.loads(questionSet)
 	rightAnswer = 0
-	engine = pyttsx.init()
+	#engine = pyttsx.init()
 	for i in range(len(questionsList)):
 		question_dict = questionsList[i]
 		print("\n\n\n{questionNo} {question}".format(questionNo = i+1, question=question_dict['q']))
-		engine.say("\n\n\n{questionNo} {question}".format(questionNo = i+1, question=question_dict['q']))
-		engine.runAndWait()
+		#engine.say("\n\n\n{questionNo} {question}".format(questionNo = i+1, question=question_dict['q']))
+		#engine.runAndWait()
 		print("a){optionA}\nb){optionB}\nc){optionC}\nd){optionD}".format(optionA = question_dict['a1'], optionB=question_dict['a2'], optionC=question_dict['a3'], optionD=question_dict['a4']))
 		answer = input("\n Enter the option (a/b/c/d) : ")
 		if question_dict['ca'] == answer:
